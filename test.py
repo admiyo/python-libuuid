26df23dd26d60740875b06104df81c0bc987fe3a
Adam Young <ayoung@redhat.com>
2012-04-20 15:15:41 -0400

    Removed unused files.

--- test.py
+++ test.py
@@ -7,11 +7,9 @@ import _uuid
 newUUID = _uuid.uuid_generate()
 print newUUID
 
-
 newUUID = _uuid.uuid_generate_time()
 print newUUID
 
 
-#libuuid.uuid_generate(uuiddef.UUID(int=0x12345678123456781234567812345678))
 
 
