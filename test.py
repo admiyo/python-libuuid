7bf8526fd7909386d174037e67bbd5cc07de4f0c
Adam Young <ayoung@redhat.com>
2012-04-20 13:22:45 -0400

    Working through build issues

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python
 
 import  sys
+import uuiddef
 import libuuid
 
-
-print libuuid.UUID('{12345678-1234-5678-1234-567812345678}')
-print libuuid.UUID('12345678123456781234567812345678')
-print libuuid.UUID('urn:uuid:12345678-1234-5678-1234-567812345678')
-print libuuid.UUID(bytes='\x12\x34\x56\x78'*4)
-print libuuid.UUID(bytes_le='\x78\x56\x34\x12\x34\x12\x78\x56' +
+print uuiddef.UUID('{12345678-1234-5678-1234-567812345678}')
+print uuiddef.UUID('12345678123456781234567812345678')
+print uuiddef.UUID('urn:uuid:12345678-1234-5678-1234-567812345678')
+print uuiddef.UUID(bytes='\x12\x34\x56\x78'*4)
+print uuiddef.UUID(bytes_le='\x78\x56\x34\x12\x34\x12\x78\x56' +
                       '\x12\x34\x56\x78\x12\x34\x56\x78')
-print libuuid.UUID(fields=(0x12345678, 0x1234, 0x5678, 0x12, 0x34, 0x567812345678))
-print libuuid.UUID(int=0x12345678123456781234567812345678)
+print uuiddef.UUID(fields=(0x12345678, 0x1234, 0x5678, 0x12, 0x34, 0x567812345678))
+print uuiddef.UUID(int=0x12345678123456781234567812345678)
 
 
+print libuuid.__dir__
+
+#libuuid.uuid_generate(uuiddef.UUID(int=0x12345678123456781234567812345678))
 
 
